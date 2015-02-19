52c709155fb0ed22e168ac01ba76f16af9ecccfa
volter9 <volter925@gmail.com>
2015-02-18 17:04:27 -0800

    Forgot to rename mysqli variables

--- actions/login.php
+++ actions/login.php
@@ -6,18 +6,18 @@ if (!$data = post_input($fields, true)) {
 	return 'Одно из полей не заполнено.';
 }
 
-$mysqli = db_connect();
+$db = db_connect();
 
 $data['password'] = md5($data['password']);
 
 $username = $data['username'];
 $password = $data['password'];
 
-if (user_exists($mysqli, $username, $password)) {
+if (user_exists($db, $username, $password)) {
 	return 'Данный пользователь уже существует';
 }
 
-if (!db_insert($mysqli, 'users', $data)) {
+if (!db_insert($db, 'users', $data)) {
 	return 'Пользователь не может быть зарегестрирован!';
 }
 
